4fe3ebd044d615d9fafb15db1366b9f3d6d4892d
Travis Tang <travis.tang@tum.de>
2021-11-20 02:52:56 +0100

    Initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return (
+        <div className="h-12 flex items-center justify-between">
+            <span className="text-lg">
+                <b className="text-warning">Sixt</b>Seconds
+            </span>
+        </div>
+    )
+}
